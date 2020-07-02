be945945b07cbdd0dc8f0a1ab0a4f1c219dbb24e
Rispolyv0n <navy.horizon.314@gmail.com>
2020-07-02 16:50:32 +0800

    Rewrite crawler and added newly crawled dataset

--- src/crawl.py
+++ src/crawl.py
@@ -1,56 +1,21 @@
 # -*- coding: UTF-8 -*-
 
-import os
-import time
-import warnings
+from shutil import copyfile
 
-import re
 import os
+import random
+import re
+import time
 import unicodedata
+import warnings
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-import requests
-import random
+
 import pandas as pd
+import requests
 
 warnings.filterwarnings("ignore")
-
-def crawl():
-    data_path = "data/tbrain_train_final_0610.csv"
-    df = pd.read_csv(data_path)
-    links = df["hyperlink"].to_list()
-
-    PATH_TO_SAVE = os.path.abspath(
-        f"{os.path.abspath(__file__)}/../../data/news/"
-    )
-
-    if not os.path.exists(PATH_TO_SAVE):
-        os.makedirs(PATH_TO_SAVE)
-
-    for idx, link in enumerate(tqdm(links)):
-        while True:
-            try:
-                response = requests.get(link)
-
-                soup = BeautifulSoup(response.text)
-                page = soup.getText()
-
-                # Reprogram whitespaces using regular expression
-                prog = re.compile(r"\s+")
-
-                tokens = prog.split(
-                    # Encode to UTF-8 and decode whole space characters to halfspace
-                    unicodedata.normalize("NFKC", page.strip()))
-                output = " ".join(tokens)
-
-                with open(f"{PATH_TO_SAVE}/{idx}.txt", "w") as f:
-                    f.write(output)
-
-                time.sleep(random.random() * 1.5 + 0.5)
-                break
-            except Exception as err:
-                time.sleep(random.random() * 1.5 + 0.5)
 
 def crawl_traverse():
     SENTENCE_LEN = 25
@@ -65,6 +30,8 @@ def crawl_traverse():
     for idx, link in enumerate(tqdm(links)):
         try:
             response = requests.get(link)
+            response.encoding = "UTF-8"
+
             soup = BeautifulSoup(response.text)
             paragraphs = []
             for child in soup.recursiveChildGenerator():
@@ -81,9 +48,14 @@ def crawl_traverse():
                     if len(paragraph) > SENTENCE_LEN:
                         paragraphs.append(paragraph)
 
-            with open(f"{PATH_TO_SAVE}/{idx}.txt", "w") as f:
-                for paragraph in paragraphs:
-                    f.write(paragraph + '\n')
+            if len(paragraphs) == 0:
+                print('empty')
+                copyfile(f"../data/news/{idx}.txt", f"{PATH_TO_SAVE}/{idx+1}.txt")
+
+            else:
+                with open(f"{PATH_TO_SAVE}/{idx+1}.txt", "w") as f:
+                    for paragraph in paragraphs:
+                        f.write(paragraph + '\n')
 
             time.sleep(random.random() * 1.5 + 0.5)
 
